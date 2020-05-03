a26ab75d9de9daad45878cf5b99302a2c3b177d0
ken <34217752+n3qik@users.noreply.github.com>
2020-05-03 16:05:38 -0400

    Update cr10leveling.js

--- octoprint_CR10_Leveling/static/cr10leveling.js
+++ octoprint_CR10_Leveling/static/cr10leveling.js
@@ -35,19 +35,19 @@ $(function() {
           {'width': '7', 'commands':
             ['M140 S0', 'M104 S0'],
           'customClass': 'btn', 'additionalClasses': 'btn-warning nowrap', 'name': 'Stop Heat', 'offset': '2'},
-          {'width': '2', 'commands':
+          {'width': '7', 'commands':
             ['G90', 'G0 Z' + settings.upper_z() + 'F500', 'G0 X' +
             settings.back_left_x() + 'Y' + settings.back_left_y() + 'F' +
             settings.feed_rate(), 'G0 Z' + settings.lower_z() + 'F500'],
           'customClass': 'btn', 'additionalClasses': 'nowrap', 'name': 'Back Left'},
            
-           {'width': '2', 'commands':
+           {'width': '7', 'commands':
             ['G90', 'G0 Z' + settings.upper_z() + 'F500', 'G0 X' +
             settings.back_center_x() + 'Y' + settings.back_center_y() + 'F' +
             settings.feed_rate(), 'G0 Z' + settings.lower_z() + 'F500'],
-          'customClass': 'btn', 'additionalClasses': 'nowrap', 'name': 'B_Center'},
+          'customClass': 'btn', 'additionalClasses': 'nowrap', 'name': 'Back Center'},
            
-          {'width': '2', 'commands':
+          {'width': '7', 'commands':
             ['G90', 'G0 Z' + settings.upper_z() + 'F500', 'G0 X' +
             settings.back_right_x() + 'Y' + settings.back_right_y() + 'F' +
             settings.feed_rate(), 'G0 Z' + settings.lower_z() + 'F500'],
@@ -57,19 +57,19 @@ $(function() {
             settings.center_x() + 'Y' + settings.center_y() + 'F' +
             settings.feed_rate(), 'G0 Z' + settings.lower_z() + 'F500'],
           'customClass': 'btn', 'additionalClasses': 'nowrap', 'name': 'Center', 'offset': '2'},
-          {'width': '2', 'commands':
+          {'width': '7', 'commands':
             ['G90', 'G0 Z' + settings.upper_z() + 'F500', 'G0 X' +
             settings.front_left_x() + 'Y' + settings.front_left_y() + 'F' +
             settings.feed_rate(), 'G0 Z' + settings.lower_z() + 'F500'],
           'customClass': 'btn', 'additionalClasses': 'nowrap', 'name': 'Front Left'},
            
-           {'width': '2', 'commands':
+           {'width': '7', 'commands':
             ['G90', 'G0 Z' + settings.upper_z() + 'F500', 'G0 X' +
             settings.front_center_x() + 'Y' + settings.front_center_y() + 'F' +
             settings.feed_rate(), 'G0 Z' + settings.lower_z() + 'F500'],
-          'customClass': 'btn', 'additionalClasses': 'nowrap', 'name': 'F_Center'},
+          'customClass': 'btn', 'additionalClasses': 'nowrap', 'name': 'Front Center'},
            
-          {'width': '2', 'commands':
+          {'width': '7', 'commands':
             ['G90', 'G0 Z' + settings.upper_z() + 'F500', 'G0 X' +
             settings.front_right_x() + 'Y' + settings.front_right_y() + 'F' +
             settings.feed_rate(), 'G0 Z' + settings.lower_z() + 'F500'],
