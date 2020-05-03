6b46e66428a5ada51e28bb04cf3dfad2961e11e5
ken <34217752+n3qik@users.noreply.github.com>
2020-05-03 15:41:07 -0400

    Update cr10leveling.js

--- octoprint_CR10_Leveling/static/cr10leveling.js
+++ octoprint_CR10_Leveling/static/cr10leveling.js
@@ -45,7 +45,7 @@ $(function() {
             ['G90', 'G0 Z' + settings.upper_z() + 'F500', 'G0 X' +
             settings.back_center_x() + 'Y' + settings.back_center_y() + 'F' +
             settings.feed_rate(), 'G0 Z' + settings.lower_z() + 'F500'],
-          'customClass': 'btn', 'additionalClasses': 'nowrap', 'name': 'Back Center'},
+          'customClass': 'btn', 'additionalClasses': 'nowrap', 'name': 'Back \xa0Center\xa0'},
            
           {'width': '2', 'commands':
             ['G90', 'G0 Z' + settings.upper_z() + 'F500', 'G0 X' +
@@ -67,7 +67,7 @@ $(function() {
             ['G90', 'G0 Z' + settings.upper_z() + 'F500', 'G0 X' +
             settings.front_center_x() + 'Y' + settings.front_center_y() + 'F' +
             settings.feed_rate(), 'G0 Z' + settings.lower_z() + 'F500'],
-          'customClass': 'btn', 'additionalClasses': 'nowrap', 'name': 'Front Center'},
+          'customClass': 'btn', 'additionalClasses': 'nowrap', 'name': 'Front \xa0Center\xa0'},
            
           {'width': '2', 'commands':
             ['G90', 'G0 Z' + settings.upper_z() + 'F500', 'G0 X' +
