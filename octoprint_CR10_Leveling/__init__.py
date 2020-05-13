54ad129f38dc7fb03f058fa37a10977194274277
egguy <crazypops@gmail.com>
2020-05-13 16:37:18 +0200

    Add python compat version

--- octoprint_CR10_Leveling/__init__.py
+++ octoprint_CR10_Leveling/__init__.py
@@ -76,6 +76,7 @@ class Cr10_levelingPlugin(octoprint.plugin.AssetPlugin,
 
 
 __plugin_name__ = "Bed Leveling Plugin"
+__plugin_pythoncompat__ = ">=2.7,<4"  # python 2 and 3
 
 
 def __plugin_load__():
